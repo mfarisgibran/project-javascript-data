1543f8463d17ad3cd3b2c7640789d3b7cb7e9049
M Faris Gibran <mfarisgibran@gmail.com>
2020-09-29 22:22:00 +0700

    Create program to determine free shipping

---
+++ freeShipping.js
@@ -0,0 +1,13 @@
+let totalPayment = 80;
+
+if (totalPayment >= 100) {
+  console.log("You're entitled for a free shipping worldwide");
+} else {
+  console.log("No free shipping for you");
+}
+
+if (totalPayment >= 100) {
+  console.log("Amout to pay is", totalPayment);
+} else {
+  console.log("Amount to pay is", totalPayment + 15);
+}
